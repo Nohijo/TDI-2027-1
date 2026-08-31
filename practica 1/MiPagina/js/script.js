4f89d964b649e5d42f1d9492e46ee7ae27f1e818
Nohijo <miguelmarquez@ciencias.unam.mx>
2026-08-31 10:37:39 -0600

    Corrige input roto y actualiza audio de ambiente en practica 1

--- practica 1/MiPagina/js/script.js
+++ practica 1/MiPagina/js/script.js
@@ -1,16 +1,16 @@
-// Botón "Ver más / Ver menos" en las tarjetas de comida
+// Botón "Ver más / Ver menos" 
 
 const botones = document.querySelectorAll('.btn-ver-mas');
 
 botones.forEach((boton) => {
-    boton.addEventListener('click', () => {
-        const descripcion = boton.previousElementSibling;
-        descripcion.classList.toggle('visible');
+    boton.addEventListener('click', () => { //Haz eso cuando click
+        const descripcion = boton.previousElementSibling;//lo dentro de la descr
+        descripcion.classList.toggle('visible');//agrega visible si no la tiene y quita si la tiene
 
         if (descripcion.classList.contains('visible')) {
             boton.textContent = 'Ver menos';
         } else {
             boton.textContent = 'Ver más';
-        }
+        }//si si se ve eentonces ver menos y al revez 
     });
 });
